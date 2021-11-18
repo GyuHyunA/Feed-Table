360aff34030b4e6246cc38acfb0f0d77fbf2fa49
Leemit <ghahn97@gmail.com>
2021-11-18 15:37:25 +0900

    add front navbar

--- src/components/front/nav/navbar.tsx
+++ src/components/front/nav/navbar.tsx
@@ -1,30 +1,48 @@
-import React from 'react'
-import { NavBarStyle } from './navstyle'
+import React from "react";
+import { NavBarStyle } from "./navstyle";
+import Link from "next/link";
 
 const list = [
-    {
-        id:"home",
-        name: "home",
-    },
-    {
-        id:"about",
-        name: "About Me",
-    },
-    {
-        id:"skill",
-        name: "Skill",
-    },
-    {
-        id:"copyright",
-        name: "copyright",
-    },
-]
+  {
+    id: "home",
+    name: "home",
+  },
+  {
+    id: "about",
+    name: "About Me",
+  },
+  {
+    id: "skill",
+    name: "Skill",
+  },
+  {
+    id: "copyright",
+    name: "copyright",
+  },
+];
 const NavBar = () => {
-    return (
-        <NavBarStyle>
-            
-        </NavBarStyle>
-    )
-}
+  return (
+    <NavBarStyle>
+      <div className="logo-wrap">
+        <Link href="/front">
+          <a>
+            <span className="logo">Leemit</span>
+          </a>
+        </Link>
+      </div>
+      <div className="list-wrap">
+        <ul className="list">
+          {list.map((v, i) => (
+            <li key={i}>
+              <Link href={`/front/${v.id}`}>
+                <a>{v.name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </NavBarStyle>
+  );
+};
 
-export default NavBar
+export default NavBar;
---
+++ src/pages/front/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+import NavBar from '../../components/front/nav/navbar'
+
+const FrontPageStyle = styled.div`
+
+`
+
+const FrontPage = () => {
+    return (
+        <FrontPageStyle>
+            <NavBar/>
+        </FrontPageStyle>
+    )
+}
+
+export default FrontPage
